16c3874feacdc5af8fb39f0915d8f0945ccb968a
matrixy <matrixy@me.com>
2018-05-22 21:19:37 +0800

    添加项目文件。

---
+++ tentacle-client/Service1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Text;
+
+namespace tentacle_client
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
